dd39158d78c8b7444bfa30c4cd8b061a64dfb033
ngoc-dan <ngocdan191026@gmail.com>
2024-11-26 12:18:10 +0700

    Add files via upload

---
+++ 6.3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int mk;
+	printf("nhap mk : ");
+	scanf("%d", &mk);
+	if(mk==2006){
+		printf(" dung rui nha ");
+	}
+	else{
+		printf(" sai mk roi ");
+	}
+	
+	return 0;
+}
+
